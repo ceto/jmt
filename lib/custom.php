24befbdd506b65e7d290ae47cc4b72685564c05c
Gabor Szabo <szabogabi@gmail.com>
2015-06-14 15:36:31 +0000

    menu kb ok

--- lib/custom.php
+++ lib/custom.php
@@ -7,4 +7,11 @@
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
-	
+	function jmt_highlight( $atts, $content = null ) {
+		return '<div class="highlight">' . do_shortcode($content) . '</div>';
+	}
+	add_shortcode('highlight', 'jmt_highlight');
+	function jmt_negative_highlight( $atts, $content = null ) {
+			return '<div class="highlight highlight--negative">' . do_shortcode($content) . '</div>';
+		}
+	add_shortcode('highlight-negative', 'jmt_negative_highlight');	
