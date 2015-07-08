0a417e9085dba3fde953ddbe7cc0df4053895929
Gábor Szabó <szabogabi@gmail.com>
2015-07-08 15:18:36 +0200

    google font link

--- templates/head.php
+++ templates/head.php
@@ -2,13 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='http://fonts.googleapis.com/css?family=Dosis:300,400,600&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-
-  <link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-
-  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-  
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700|Dosis:300,400,600&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
